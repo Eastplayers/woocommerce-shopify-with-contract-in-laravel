c5977b759fda242ae9743a36f802e9e8977f6e4d
esp-sonnt <son.nguyen@eastplayers.io>
2021-02-20 15:49:27 +0700

    get products with woocommerce api

--- app/Services/Woocommerce/Product/GetProduct.php
+++ app/Services/Woocommerce/Product/GetProduct.php
@@ -3,14 +3,31 @@
 namespace App\Services\Woocommerce\Product;
 
 use App\Contracts\Product\GetProductInterface;
+use Automattic\WooCommerce\Client;
+use Automattic\WooCommerce\HttpClient\HttpClientException;
 
 /**
- *
- */
+*
+*/
 class GetProduct implements GetProductInterface
 {
     public function getData(int $page, int $pageSize = 10)
     {
         echo "You get Woocommerce Product\n";
+        $woocommerce = new Client(
+            env('WOOCOMMERCE_STORE_URL', ''),
+            env('WOOCOMMERCE_CONSUMER_KEY', ''),
+            env('WOOCOMMERCE_CONSUMER_SECRET', ''),
+            [
+                'wp_api' => true,
+                'version' => 'wc/v3',
+                'query_string_auth' => true
+            ]
+        );
+        $products = $woocommerce->get('products', array(
+            'per_page' => $pageSize,
+            'page' => $page
+        ));
+        dd($products);
     }
 }
